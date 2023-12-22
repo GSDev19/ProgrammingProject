7db0d4b8b29736397d02b5277173e8167b943539
German <germza123@gmail.com>
2023-12-21 21:52:41 -0300

    Your commit message

--- Programming/Assets/Scripts/Objects/NewPowerObject.cs
+++ Programming/Assets/Scripts/Objects/NewPowerObject.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewPowerObject : MonoBehaviour
 {
+    public static Action<Element> OnPickedNewPower;
     public Element element;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            GameManager.Instance.HandleUnlockedElements(element, true);
+            OnPickedNewPower.Invoke(element);
+            //GameManager.Instance.HandleUnlockedElements(element, true);
             this.gameObject.SetActive(false);
         }
     }
